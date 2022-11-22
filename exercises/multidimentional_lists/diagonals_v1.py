61d2f66cdc6149a3c6558128eb687bfe4f9048a6
Rositsa Ilieva-Kitova <105305366+RosiIIK@users.noreply.github.com>
2022-11-22 19:38:18 +0200

    Create diagonals_v1.py

---
+++ exercises/multidimentional_lists/diagonals_v1.py
@@ -0,0 +1,14 @@
+size = int(input())
+matrix = []
+
+for _ in range(size):
+    matrix.append([int(el) for el in input().split(", ")])
+
+primary_diagonal = []
+secondary_diagonal = []
+for idx in range(size):
+    primary_diagonal.append(matrix[idx][idx])
+    secondary_diagonal.append(matrix[idx][size - 1 - idx])
+
+print(f"Primary diagonal: {', '.join([str(x) for x in primary_diagonal])}. Sum: {sum(primary_diagonal)}")
+print(f"Secondary diagonal: {', '.join([str(x) for x in secondary_diagonal])}. Sum: {sum(secondary_diagonal)}")
