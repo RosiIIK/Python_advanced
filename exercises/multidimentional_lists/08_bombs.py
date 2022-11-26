6e7c5d90e05121ea8e1c250b2da205dcb78819c1
Rositsa Ilieva-Kitova <105305366+RosiIIK@users.noreply.github.com>
2022-11-26 15:04:39 +0200

    Create 08_bombs.py

---
+++ exercises/multidimentional_lists/08_bombs.py
@@ -0,0 +1,67 @@
+#not ready - 90%
+def is_inside(row, col, size):
+    return 0 <= row < size and 0 <= col < size
+
+def get_neighbours(row, col, matrix):
+    # row - 1, col
+    size = len(matrix)
+    neighbours = []
+    if is_inside(row - 1, col, size) and matrix[row - 1][col]:
+        neighbours.append([row - 1, col])
+    # row + 1, col
+    if is_inside(row + 1, col, size) and matrix[row + 1][col]:
+        neighbours.append([row + 1, col])
+    # row, col - 1
+    if is_inside(row, col - 1, size) and matrix[row][col - 1]:
+        neighbours.append([row, col - 1])
+    # row, col + 1
+    if is_inside(row, col + 1, size) and matrix[row][col + 1]:
+        neighbours.append([row, col + 1])
+    # row - 1, col - 1
+    if is_inside(row - 1, col - 1, size) and matrix[row - 1][col - 1]:
+        neighbours.append([row - 1, col - 1])
+    # row - 1, col + 1
+    if is_inside(row - 1, col + 1, size) and matrix[row - 1][col + 1]:
+        neighbours.append([row - 1, col + 1])
+    # row + 1, col - 1
+    if is_inside(row + 1, col - 1, size) and matrix[row + 1][col - 1]:
+        neighbours.append([row + 1, col - 1])
+    # row + 1, col + 1
+    if is_inside(row + 1, col + 1, size) and matrix[row + 1][col + 1]:
+        neighbours.append([row + 1, col + 1])
+    return neighbours
+
+n = int(input())
+
+matrix = []
+
+for _ in range(n):
+    matrix.append([int(el)for el in input().split()])
+
+bombs = input().split()
+
+for bomb_coords in bombs:
+    bomb_row, bomb_col = [int(el) for el in bomb_coords.split(',')]
+
+    if matrix[bomb_row][bomb_col] <= 0:
+        continue
+
+    bomb_power = matrix[bomb_row][bomb_col]
+    matrix[bomb_row][bomb_col] = 0
+
+    for row, col in get_neighbours(bomb_row, bomb_col, matrix):
+        matrix[row][col] -= bomb_power
+
+alive_cells = 0
+alive_cells_sum = 0
+for row in matrix:
+    for el in row:
+        if el > 0:
+            alive_cells += 1
+            alive_cells_sum += el
+
+print(f"Alive cells: {alive_cells}")
+print(f"Sum: {alive_cells_sum}")
+
+for row in matrix:
+    print(*row,sep=' ')
