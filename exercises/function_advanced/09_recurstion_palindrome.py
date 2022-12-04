c6719e2c306e48a854d72210e82d7efdde38ad91
Rositsa Ilieva-Kitova <105305366+RosiIIK@users.noreply.github.com>
2022-12-04 14:14:06 +0200

    Create 09_recurstion_palindrome.py

---
+++ exercises/function_advanced/09_recurstion_palindrome.py
@@ -0,0 +1,13 @@
+#break point / incrementing case
+
+def palindrome(word, idx):
+    if idx == len(word) // 2:
+        return f"{word} is a palindrome"
+
+    first, last = word[idx], word[-1 -idx]
+    if first != last:
+        return f"{word} is not a palindrome"
+
+    return palindrome(word, idx + 1)
+
+print(palindrome("abcba", 0))
