09eec03a31a2092bdb4416447a2b29bbdbf0d911
Rositsa Ilieva-Kitova <105305366+RosiIIK@users.noreply.github.com>
2022-12-04 14:11:44 +0200

    Create 05_concatenate.py

---
+++ exercises/function_advanced/05_concatenate.py
@@ -0,0 +1,7 @@
+def concatenate(*args, **kwargs):
+    word = ''.join(args)
+    for key, val in kwargs.items():
+        word = word.replace(key, val)
+    return word
+
+print(concatenate("Soft", "UNI", "Is", "Grate", "!", UNI="Uni", Grate="Great"))
